7ed55fb7f29e0a0a0c9840e94dce9d88163f9219
Xavier-Do <xdo@odoo.com>
2019-05-02 16:22:38 +0200

    Data to add ruodoo repo for runbot

---
+++ runbotdata/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "runbotdata",
+    'summary': "Runbot data",
+    'description': "Runbot repo data",
+    'author': "Odoo SA",
+    'website': "http://runbot.odoo.com",
+    'category': 'Website',
+    'version': '3.1',
+    'depends': ['runbot'],
+    'data': [
+        'data/repo_data.xml',
+    ],
+}
